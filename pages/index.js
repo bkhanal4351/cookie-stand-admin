f5f13f2541116033b31b0e91790ae949e53fe838
Bishal Khanal <bishal.khanal87@gmail.com>
2022-06-08 20:50:17 -0700

    create components, still need to work on rendering

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+import createForm from '../components/CreateForm'
+
+
 import { useState } from 'react'
 
 
