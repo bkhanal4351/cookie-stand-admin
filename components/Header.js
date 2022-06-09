7bfe5cd80675fa6dbbb39e8424f60139038cd2d4
Bishal Khanal <bishal.khanal87@gmail.com>
2022-06-09 16:26:40 -0700

    completed lab

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
 export default function Header(){
   return (
-  <header className="px-8 py-6 text-4xl bg-emerald-500 text-black">
+  <header className="px-8 py-6 text-4xl bg-emerald-500 text-black flex justify-between">
     <h1>Cookie Stand Admin</h1>
+    <button className='bg-gray-300 w-24 hover:bg-sky-700 text-sm rounded-md ml-auto mr-6'>Overview</button>
   </header>
   );
 }
